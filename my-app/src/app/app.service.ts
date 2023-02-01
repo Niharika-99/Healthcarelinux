2b2e44b75ed3516d0fdc7436537f3c5910394bde
Niharika Narayana(UST <198792@ust-global.com>
2023-02-01 12:32:17 +0530

    first commit

---
+++ my-app/src/app/app.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class AppService {
+
+  constructor(private http: HttpClient) { }
+
+
+
+  rootURL = 'http://10.31.114.21:5000';
+  // rootURL = 'http://127.0.0.1:5000/';
+  liveBaseUrl = 'http://20.228.170.193:8080'
+
+  getUsers() {
+    return this.http.get(this.rootURL + '/users');
+  }
+
+  getGridData(gridCount: number, searchText: string) {
+console.log(this.rootURL + `/icd10/${searchText}`);
+    //return this.http.get(this.rootURL + `/drugInfo/${gridCount}?searchText=${searchText}`);
+    if (gridCount == 1) {
+      return this.http.get(this.rootURL + `/icd10/${searchText}`);
+    }
+     else if (gridCount == 2) {
+      return this.http.get(this.rootURL + `/conceptIDs/${searchText}`);
+    }
+     else if (gridCount == 3) {
+      return this.http.get(this.rootURL + `/icd10/${searchText}`);
+    }
+     else if (gridCount == 4) {
+      return this.http.get(this.rootURL + `/NDCs/${searchText}`);
+    }
+    // if (gridCount == 1) {
+    //      const icd10 = this.http.get(this.rootURL + `/HealthAPIv2/ICD10v2/${searchText}`)
+    //   return icd10 ;
+    // }
+    //  else if (gridCount == 2) {
+    //   return this.http.get(this.rootURL + `/HealthAPIv2/ConceptIDs/${searchText}`);
+    // }
+    //  else if (gridCount == 3) {
+    //   return this.http.get(this.rootURL + `/HealthAPIv2/ICD10v2/${searchText}`);
+    // }
+    //  else if (gridCount == 4) {
+    //   return this.http.get(this.rootURL + `/HealthAPIv2/NDCs/${searchText}`);
+    // }
+  }
+  getGridDataLive(gridName: string) {
+
+    return this.http.get(this.rootURL + `/health/${gridName}`.toString());
+
+  }
+  addUser(user: any) {
+    return this.http.post(this.rootURL + '/user', { user });
+  }
+
+}
